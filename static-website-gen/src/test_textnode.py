f8370506f7520d30b7afdc17724c22a575fc1b21
Harii2K4 <harikishan2k4@gmail.com>
2026-08-05 18:22:40 +0530

    Implements conversion of the textnode to htmlnode(leafnode) with tests passing

--- static-website-gen/src/test_textnode.py
+++ static-website-gen/src/test_textnode.py
@@ -1,32 +1,84 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
     def test_equal(self):
-        t1 = TextNode(content="Hello world", type="text")
-        t2 = TextNode(content="Hello world", type="text")
+        t1 = TextNode(content="Hello world", type=TextType.TEXT)
+        t2 = TextNode(content="Hello world", type=TextType.TEXT)
         self.assertEqual(t1, t2)
 
     def test_notequal(self):
-        t1 = TextNode(content="Hello world", type="text")
-        t2 = TextNode(content="this aint equal dawg", type="text")
+        t1 = TextNode(content="Hello world", type=TextType.TEXT)
+        t2 = TextNode(content="this aint equal dawg", type=TextType.TEXT)
         self.assertNotEqual(t1, t2)
 
     def test_no_url(self):
         t1 = TextNode(
-            content="Hello world", type="link", link="https://www.boot.dev/lessons"
+            content="Hello world",
+            type=TextType.LINK,
+            link="https://www.boot.dev/lessons",
         )
-        t2 = TextNode(content="Hello world", type="link")
+        t2 = TextNode(content="Hello world", type=TextType.LINK)
         self.assertNotEqual(t1, t2)
 
     def test_diff_text_type(self):
-        t1 = TextNode(content="Hello world", type="Bold")
-        t2 = TextNode(content="Hello world", type="text")
+        t1 = TextNode(content="Hello world", type=TextType.BOLD)
+        t2 = TextNode(content="Hello world", type=TextType.TEXT)
         self.assertNotEqual(t1, t2)
 
     def test_invalid_object_type(self):
-        t1 = TextNode(content="Hello world", type="Bold")
+        t1 = TextNode(content="Hello world", type=TextType.BOLD)
         with self.assertRaises(TypeError):
             self.assertNotEqual(t1, "Hello World")
+
+
+class ConvertTextNode(unittest.TestCase):
+    def test_text_node_to_html_node_TEXT(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_node_to_html_node_LINK(self):
+        node = TextNode(
+            "About The Odin Project",
+            TextType.LINK,
+            link="https://www.theodinproject.com/about",
+        )
+        html_node = text_node_to_html_node(node)
+        res = (
+            '<a href="https://www.theodinproject.com/about">About The Odin Project</a>'
+        )
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "About The Odin Project")
+        self.assertEqual(res, html_node.to_html())
+
+    def test_text_node_to_html_node_BOLD(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "strong")
+        res = "<strong>This is a bold text node</strong>"
+        self.assertEqual(res, html_node.to_html())
+
+    def test_text_node_to_html_node_ITALIC(self):
+        node = TextNode("This is an italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "em")
+        res = "<em>This is an italic text node</em>"
+        self.assertEqual(res, html_node.to_html())
+
+    def test_text_node_to_html_node_CODE(self):
+        node = TextNode("This is a code text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        res = "<code>This is a code text node</code>"
+        self.assertEqual(res, html_node.to_html())
+
+    def test_text_node_to_html_node_IMAGE(self):
+        node = TextNode("Cute cat image", TextType.IMAGE, link="../cat.img")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        res = '<img src="../cat.img" alt="Cute cat image">'
+        self.assertEqual(res, html_node.to_html())
--- static-website-gen/src/textnode.py
+++ static-website-gen/src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -14,7 +16,7 @@ class TextNode:
     def __init__(
         self,
         content: str,
-        type: str,
+        type: TextType,
         link: str | None = None,
     ) -> None:
 
@@ -27,10 +29,34 @@ class TextNode:
             raise TypeError(f"other should be of type Textnode not {type(other)}")
 
         isContentEqual = self.content == other.content
-        isTypeEqual = self.type == other.type
+        isTypeEqual = self.type.value == other.type.value
         isLinkEqual = self.link == other.link
 
         return isContentEqual and isLinkEqual and isTypeEqual
 
     def __repr__(self) -> str:
-        return f"TextNode({self.content}, {self.type}, {self.link})"
+        return f"TextNode({self.content}, {self.type.value}, {self.link})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    match text_node.type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.content)
+        case TextType.BOLD:
+            return LeafNode(tag="strong", value=text_node.content)
+        case TextType.ITALIC:
+            return LeafNode(tag="em", value=text_node.content)
+        case TextType.LINK:
+            return LeafNode(
+                tag="a", attributes={"href": text_node.link}, value=text_node.content
+            )
+        case TextType.IMAGE:
+            return LeafNode(
+                tag="img",
+                attributes={"src": text_node.link, "alt": text_node.content},
+                value="",
+            )
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.content)
+        case _:
+            raise TypeError(f"Not a valid text type {text_node.type}")
