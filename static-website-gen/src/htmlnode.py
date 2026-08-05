9ea286c5cfb653bb13455316b15bd731ab15e412
Harii2K4 <harikishan2k4@gmail.com>
2026-08-05 18:22:09 +0530

    implements leafnode and parentnode with tests passing

--- static-website-gen/src/htmlnode.py
+++ static-website-gen/src/htmlnode.py
@@ -1,4 +1,3 @@
-from shlex import join
 from typing import Any
 
 
@@ -36,7 +35,83 @@ class LeafNode(HtmlNode):
         super().__init__(tag, value, None, attributes)
 
     def to_html(self) -> str:
-        assert self.value
         if not self.tag:
+            assert self.value
             return self.value
+        if self.tag == "img":
+            return f"<{self.tag}{self.props_to_html()}>"
+        assert self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        content = ""
+        if self.tag:
+            content += f"\ntag:{self.tag},"
+        content += f"\nvalue:{self.value},"
+        if self.attributes:
+            content += f"\nattr:{self.attributes}"
+        return f"LeafNode({content}\n)"
+
+
+class ParentNode(HtmlNode):
+    def __init__(
+        self,
+        tag: str,
+        children: list[HtmlNode],
+        attributes: dict[str, Any] | None = None,
+    ) -> None:
+        super().__init__(tag, None, children, attributes)
+
+    def __repr__(self) -> str:
+        content = f"\ntag:{self.tag},"
+        if self.value:
+            content += f"\nvalue:{self.value}"
+        if self.attributes:
+            content += f"\nattr:{self.attributes}"
+        content += f"\nchildren:{self.attributes}"
+        return f"ParentNode({content}\n)"
+
+    def to_html(self) -> str:
+        if not self.tag:
+            raise ValueError("Parent Nodes must have a tag")
+        if not self.children:
+            raise ValueError("Parent Nodes must have a children")
+
+        if self.attributes:
+            output = f"<{self.tag}{self.props_to_html()}>"
+        else:
+            output = f"<{self.tag}>"
+
+        for child in self.children:
+            output += child.to_html()
+
+        return output + f"</{self.tag}>"
+
+
+list_ul = ParentNode(
+    tag="ul",
+    children=[
+        LeafNode("li", value="Item 1"),
+        LeafNode("li", value="Item 2"),
+        LeafNode("li", value="Item 3"),
+        LeafNode("li", value="Item 4"),
+        LeafNode("li", value="Item 5"),
+    ],
+    attributes={"style": "color:red;list-style:None"},
+)
+h1 = ParentNode(
+    tag="p",
+    children=[
+        LeafNode(None, value="This Is "),
+        LeafNode("b", value="Amazing "),
+        LeafNode(None, value="Bro"),
+    ],
+)
+body = ParentNode(
+    tag="body", children=[h1, list_ul], attributes={"style": "background-color:black"}
+)
+head = ParentNode(tag="head", children=[LeafNode("title", value="Statics")])
+html = ParentNode(tag="html", children=[head, body])
+
+with open("test.html", "w") as f:
+    f.write(html.to_html())
--- static-website-gen/src/test_htmlnode.py
+++ static-website-gen/src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HtmlNode, LeafNode
+from htmlnode import HtmlNode, LeafNode, ParentNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -54,3 +54,72 @@ class TestLeafNode(unittest.TestCase):
             value="Hello world", tag=None, attributes={"class": "first"}
         )
         self.assertEqual("Hello world", leaf_node.to_html())
+
+
+class TestParentNode(unittest.TestCase):
+    def test_parentNode_to_html_with_children_1(self):
+        h = ParentNode(
+            tag="p",
+            children=[
+                LeafNode(None, value="This is normal text "),
+                LeafNode("b", value="This is in bold"),
+                LeafNode(None, value=" This is normal text"),
+            ],
+        )
+        res = "<p>This is normal text <b>This is in bold</b> This is normal text</p>"
+        self.assertEqual(res, h.to_html())
+
+    def test_parentNode_to_html_with_children_2(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_parentNode_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_parentNode_to_html_no_children(self):
+        with self.assertRaises(ValueError):
+            ParentNode(tag="body", children=[]).to_html()
+
+    def test_parentNode_to_html_no_tag(self):
+        with self.assertRaises(ValueError):
+            ParentNode(
+                tag=None, children=[LeafNode(tag="b", value="this wont run")]
+            ).to_html()
+
+    def test_parent_to_html_full(self):
+        res = '<html><head><title>Statics</title></head><body style="background-color:black"><p>This Is <b>Amazing </b>Bro</p><ul style="color:red;list-style:None"><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li><li>Item 5</li></ul></body></html>'
+        list_ul = ParentNode(
+            tag="ul",
+            children=[
+                LeafNode("li", value="Item 1"),
+                LeafNode("li", value="Item 2"),
+                LeafNode("li", value="Item 3"),
+                LeafNode("li", value="Item 4"),
+                LeafNode("li", value="Item 5"),
+            ],
+            attributes={"style": "color:red;list-style:None"},
+        )
+        h1 = ParentNode(
+            tag="p",
+            children=[
+                LeafNode(None, value="This Is "),
+                LeafNode("b", value="Amazing "),
+                LeafNode(None, value="Bro"),
+            ],
+        )
+        body = ParentNode(
+            tag="body",
+            children=[h1, list_ul],
+            attributes={"style": "background-color:black"},
+        )
+        head = ParentNode(tag="head", children=[LeafNode("title", value="Statics")])
+        html = ParentNode(tag="html", children=[head, body])
+
+        self.assertTrue(res, html.to_html())
